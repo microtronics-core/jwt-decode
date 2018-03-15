bae79e98b2e837b683ed39f111bda955dd9f8c8c
lusc <lusc@microtronics.com>
2018-03-15 11:46:15 +0100

    inital

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+let jwt = require('jsonwebtoken');
+
+let jwtDecode = function (options) {
+  console.log(options);
+
+  //todo: get some parameter like keyserver etc.
+  //todo: verify token/complete request with info from keyservice ...
+};
+
+jwtDecode.prototype.verifyToken = function (token, publicKey) {
+  let decoded = '';
+  try {
+    decoded = jwt.verify(token, publicKey);
+  }
+  catch(err) {
+    console.error(err);
+  }
+
+  console.log(decoded);
+
+  return decoded;
+};
+
+//module.exports = jwtDecode;
+module.exports = function (options) {
+  return new jwtDecode(options);
+};
+
+
