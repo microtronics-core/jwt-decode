c92cd300e1898b49ab5efdd596ea0fda737a7f2a
lusc <lusc@microtronics.com>
2018-03-19 13:29:19 +0100

    inital

--- index.js
+++ index.js
@@ -1,26 +1,75 @@
 'use strict';
 
 let jwt = require('jsonwebtoken');
+let request = require('request-json');
+let parseToken = require('parse-bearer-token');
+let Validator = require('jsonschema').Validator;
 
-let jwtDecode = function (options) {
-  console.log(options);
-
-  //todo: get some parameter like keyserver etc.
-  //todo: verify token/complete request with info from keyservice ...
+let optionsSchema = {
+  'type': 'object',
+  'properties': {
+    'KeyServer': {'type': 'string'},
+    'ApplianceId': {'type': 'string'}
+  },
+  'required': ['KeyServer', 'ApplianceId']
 };
 
-jwtDecode.prototype.verifyToken = function (token, publicKey) {
-  let decoded = '';
+let jwtDecode = function (options) {
   try {
-    decoded = jwt.verify(token, publicKey);
+    let v = new Validator();
+    v.validate(options, optionsSchema, {throwError: true});
   }
-  catch(err) {
-    console.error(err);
+  catch (error) {
+    console.error('JSON-Schema validation failed: ' + error);
   }
 
-  console.log(decoded);
+  this._keyServer = options.KeyServer;
+  this._applianceId = options.ApplianceId;
 
-  return decoded;
+  this._client = request.createClient(options.KeyServer);
+};
+
+jwtDecode.prototype._requestPublicKey = function (onDone) {
+  let path = '/api/1/keys/' + this._applianceId;
+  this._client.get(path, (err, res, body) => {
+    if(err) {
+      console.error('Request to ' + this._keyServer + path + ' failed: ' + err);
+      onDone(null);
+    }
+    else {
+      onDone(body.PublicKey);
+    }
+  });
+};
+
+jwtDecode.prototype.verifyToken = function (token, onDone) {
+  this._requestPublicKey((publicKey) => {
+    jwt.verify(token, publicKey, (err, decoded) => {
+      if(err) {
+        console.error(err);
+        onDone(err);
+      }
+      else {
+        onDone(null, decoded);
+      }
+    });
+  });
+};
+
+jwtDecode.prototype.verifyRequest = function (req, onDone) {
+  this._requestPublicKey((publicKey) => {
+    let token = parseToken(req);
+    jwt.verify(token, publicKey, (err, decoded) => {
+      if(err) {
+        console.error(err);
+        onDone(err);
+      }
+      else {
+        req.decoded = decoded;
+        onDone(null);
+      }
+    });
+  });
 };
 
 //module.exports = jwtDecode;
