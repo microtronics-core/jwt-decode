3cf563d4bc8cf19b5a6283452296f9146b41d15d
hr <hr@microtronics.at>
2018-09-20 15:01:57 +0200

    added .gitignore file changed verification mechanism of token

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 'use strict';
 
-const jwt = require('jsonwebtoken');
 const request = require('request-json');
 const parseToken = require('parse-bearer-token');
 const Validator = require('jsonschema').Validator;
+const sshpk = require('sshpk');
 
 const optionsSchema = {
   'type': 'object',
   'properties': {
     'KeyServer': {'type': 'string'}
-  },
-  'required': ['KeyServer']
+  }
 };
 
 let jwtDecode = function (options) {
@@ -22,7 +21,7 @@ let jwtDecode = function (options) {
     console.error('JSON-Schema validation failed: ' + error); //TODO: throw error???
   }
 
-  this._keyServer = options.KeyServer;
+  this._keyServer = options.KeyServer || "https://keyservice.microtronics.com";
 
   this._client = request.createClient(options.KeyServer);
 };
@@ -56,7 +55,7 @@ jwtDecode.prototype.verifyRequest = function (req) {
 
 function requestPublicKey(client, token) {
   return new Promise((resolve, reject) => {
-    let payload = JSON.parse(new Buffer(token.substring(token.indexOf('.') + 1, token.lastIndexOf('.')), 'base64'));
+    let payload = JSON.parse(new Buffer(token.split('.')[1], 'base64'));
     let path = '/api/1/keys/' + payload.orig;   // payload.orig = applianceid
     client.get(path, (err, res, body) => {
       if(err)
@@ -69,12 +68,27 @@ function requestPublicKey(client, token) {
 
 function verifyToken(token, publicKey) {
   return new Promise((resolve, reject) => {
-    jwt.verify(token, Buffer.from(publicKey, 'base64'), (err, decoded) => {
-      if (err)
-        reject(err);
-      else
-        resolve(decoded);
-    });
+      let pubKey=new sshpk.Key({
+          type: 'ed25519',
+          parts: [
+              { name: 'A', data: Buffer.from(publicKey, 'hex')}
+          ]
+      });
+      let tokenParts=token.split('.');
+      let signature = new sshpk.Signature({
+        type: 'ed25519',
+        hashAlgorithm: 'sha512',
+        parts: [
+            { name: 'sig', data: Buffer.from(tokenParts[2], 'base64')}
+        ]
+      });
+      let verifier=pubKey.createVerify();
+      verifier.update(`${tokenParts[0]}.${tokenParts[1]}`);
+      if (verifier.verify(signature)) {
+          resolve();
+      } else {
+          reject();
+      }
   });
 }
 
